66838fca70ec2f199121f152c71a955a205f27f4
Hugo Benitez <hugo.boca16@gmail.com>
2024-04-18 14:45:17 -0300

    modifique apariencia de botones

--- ABM Productos/Solucion01/Form1.Designer.cs
+++ ABM Productos/Solucion01/Form1.Designer.cs
@@ -39,45 +39,65 @@
             // 
             // Listas
             // 
+            this.Listas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.Listas.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.Listas.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.Listas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Listas.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Listas.ForeColor = System.Drawing.Color.White;
             this.Listas.Location = new System.Drawing.Point(109, 126);
             this.Listas.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Listas.Name = "Listas";
-            this.Listas.Size = new System.Drawing.Size(275, 26);
+            this.Listas.Size = new System.Drawing.Size(275, 36);
             this.Listas.TabIndex = 0;
             this.Listas.Text = "LISTA ARTÍCULOS";
-            this.Listas.UseVisualStyleBackColor = true;
+            this.Listas.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
             this.button2.Location = new System.Drawing.Point(558, 212);
             this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(275, 26);
+            this.button2.Size = new System.Drawing.Size(275, 36);
             this.button2.TabIndex = 1;
             this.button2.Text = "MODIFICAR";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // BUSCAR
             // 
+            this.BUSCAR.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.BUSCAR.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.BUSCAR.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.BUSCAR.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.BUSCAR.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BUSCAR.ForeColor = System.Drawing.Color.White;
             this.BUSCAR.Location = new System.Drawing.Point(558, 126);
             this.BUSCAR.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.BUSCAR.Name = "BUSCAR";
-            this.BUSCAR.Size = new System.Drawing.Size(275, 26);
+            this.BUSCAR.Size = new System.Drawing.Size(275, 36);
             this.BUSCAR.TabIndex = 2;
             this.BUSCAR.Text = "BUSCAR";
-            this.BUSCAR.UseVisualStyleBackColor = true;
+            this.BUSCAR.UseVisualStyleBackColor = false;
             // 
             // button4
             // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.White;
             this.button4.Location = new System.Drawing.Point(109, 212);
             this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(275, 26);
+            this.button4.Size = new System.Drawing.Size(275, 36);
             this.button4.TabIndex = 3;
             this.button4.Text = "AGREGAR ARTÍCULOS";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // label1
             // 
@@ -92,23 +112,34 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Location = new System.Drawing.Point(109, 300);
             this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(275, 26);
+            this.button1.Size = new System.Drawing.Size(275, 36);
             this.button1.TabIndex = 5;
             this.button1.Text = "ELIMINAR\r\n";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.White;
             this.button3.Location = new System.Drawing.Point(558, 300);
             this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(275, 26);
+            this.button3.Size = new System.Drawing.Size(275, 36);
             this.button3.TabIndex = 6;
             this.button3.Text = "DETALLE DE UN ARTÍCULO ";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -122,6 +153,7 @@
             this.Controls.Add(this.BUSCAR);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.Listas);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
