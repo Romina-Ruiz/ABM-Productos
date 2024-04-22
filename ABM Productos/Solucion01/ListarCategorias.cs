0a2a95d9fbba40c65292e92cee1cee9fa056dbdd
jonatan <jonatan_lazarte1992@hotmail.com>
2024-04-22 02:04:29 -0300

    Agrego pantalla listarCategorias con funcionalidad

---
+++ ABM Productos/Solucion01/ListarCategorias.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using negocio;
+
+namespace dominio
+{
+    public partial class ListarCategorias : Form
+    {
+        public ListarCategorias()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvListaCategoria_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        { 
+            
+
+        }
+
+        private void ListarCategorias_Load(object sender, EventArgs e)
+        {
+            CategoriaNegocio negocio1 = new CategoriaNegocio();
+            dgvListaCategoria.DataSource = negocio1.listar();
+        }
+
+        private void btnAtras_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
